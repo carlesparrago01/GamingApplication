8db1a8ac607bd97b0f29d3c63d117c051f3dff04
carlesparrago01 <carl.esparrago01@gmail.com>
2024-04-23 19:39:51 +0800

    fix

---
+++ app/src/main/java/ph/com/carlesparrago/gamingApp/APIs/models/Banner.java
@@ -0,0 +1,23 @@
+package ph.com.carlesparrago.gamingApp.APIs.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Banner{
+        @SerializedName("og")
+        String og;
+        @SerializedName("sm")
+        String sm;
+
+        public Banner(String og, String sm) {
+            this.og = og;
+            this.sm = sm;
+        }
+
+        public String getOg() {
+            return og;
+        }
+
+        public String getSm() {
+            return sm;
+        }
+    }
